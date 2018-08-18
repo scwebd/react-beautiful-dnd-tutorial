dcd0cfc6294a1d7e7712ce756d62d0e5d503c5f8
scwebd <scwebd@gmail.com>
2018-08-17 23:16:49 -0600

    Persisting new array order after drag

--- src/index.js
+++ src/index.js
@@ -11,6 +11,35 @@ class App extends Component {
 
     onDragEnd = result => {
         // ?????
+        const { destination, source, draggableId } = result;
+
+        if (!destination) {
+            return false;
+        }
+
+        if (destination.droppableId === source.droppableId && destination.index === source.index) {
+            return false;
+        }
+
+        const column = this.state.columns[source.droppableId];
+        const newTaskIds = Array.from(column.taskIds);
+        newTaskIds.splice(source.index, 1);
+        newTaskIds.splice(destination.index, 0, draggableId);
+
+        const newColumn = {
+            ...column,
+            taskIds: newTaskIds
+        }
+
+        const newState = {
+            ...this.state,
+            columns: {
+                ...this.state.columns,
+                [newColumn.id]: newColumn
+            }
+        }
+
+        this.setState(newState);
     }
 
     render() {
